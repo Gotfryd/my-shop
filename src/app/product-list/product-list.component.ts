d5ee9dfe4dd19ee04f13a1c52aba96cb6b0b6285
Bartlomiej Gotfryd <barto.gotfryd33@gmail.com>
2025-08-04 21:39:24 +0200

    Add/remove to cart buttons with event binding added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,20 +4,40 @@ import { Component } from '@angular/core';
   selector: 'app-product-list',
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductList {
-  userName = 'Jack Sparrow';
+  //userName = 'Jack Sparrow';
+  addToCart: number = 0;
   product = {
     name: 'Samsung Galaxy S21',
     price: 499,
     color: 'Snow White',
     discountValue: 12.5,
     inStock: 3,
-    productImage: '/samasung.jpg'
+    productImage: '/samasung.jpg',
+  };
+
+  getDiscountedPrice() {
+    return (
+      this.product.price -
+      (this.product.price * this.product.discountValue) / 100
+    );
   }
 
-  getDiscountedPrice(){
-    return this.product.price-(this.product.price*this.product.discountValue/100)
+  onNameChange(event: any) {
+    // this.userName = event.target.value;
+  }
+
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if (this.product.inStock > this.addToCart) {
+      this.addToCart++;
+    }
   }
 }
